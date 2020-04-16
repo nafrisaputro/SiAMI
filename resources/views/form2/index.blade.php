80ae46577a9f6d00e33bff12a154a9073cdd3683
dwiki48 <adnsyhdwiki69@gmail.com>
2020-04-16 18:52:24 +0700

    625

--- resources/views/form2/index.blade.php
+++ resources/views/form2/index.blade.php
@@ -12,27 +12,73 @@
             <h1 class="h1 display">Data Form 2</h1>
         </header>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <div class="card">
                     <div class="card-header">
                         <h1 class="h1 display">Form 2
                         </h1>
                     </div>
                     <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>NAMA JURUSAN</th>
-                                        <th>TOMBOL AKSI</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                    <div class="nav-tabs-custom">
+    							<ul class="nav nav-tabs">
+    								<li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true">A</a></li>
+    								<li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">B</a></li>
+    							</ul>
+    						</br>
+    						<div class="tab-content">
+    							<div class="tab-pane active" id="tab_1">
+    								
+    									<div class="row">
+    										<div class="col-md-6">
+    											<div class="box box-primary">
+    													@csrf
+    												<br>
+    												{{-- NIK --}}
+    												<div class="form-group">
+    													<label >NIK</label>
+    													<input required id="nik" name="NIK" type="number" class="form-control" id="">
+    												</div>
+    											</div>
+    										</div>
+    										<div class="col-md-6">
+    											<div class="box box-primary">
+    												<br>
+    												{{-- alamat --}}
+    												<div class="form-group">
+    													<label>Alamat</label>
+    													<textarea required name="ALAMAT" class="form-control" rows="3" placeholder=""></textarea>
+    												</div>          
+    											</div>
+    										</div>
+    									</div>
+    								</div>
+    								<!-- /.tab-pane -->
+    								<div class="tab-pane" id="tab_2">
+                                    <div class="row">
+    										<div class="col-md-6">
+    											<div class="box box-primary">
+    													@csrf
+    												<br>
+    												{{-- NIK --}}
+    												<div class="form-group">
+    													<label >NIK</label>
+    													<input required id="nik" name="NIK" type="number" class="form-control" id="">
+    												</div>
+    											</div>
+    										</div>
+    										<div class="col-md-6">
+    											<div class="box box-primary">
+    												<br>
+    												{{-- alamat --}}
+    												<div class="form-group">
+    													<label >NIK</label>
+    													<input required id="nik" name="NIK" type="number" class="form-control" id="">
+    												</div>          
+    											</div>
+    										</div>
+    									</div>
+    								</div>
+    								
                     </div>
                 </div>
             </div>
